b6603c5fc91425916b1d42ba20fff882eb1123b4
geermc4 <geermc4@gmail.com>
2016-05-12 14:01:30 -0700

    tmp fix

--- lib/subdomain_fu/url_rewriter.rb
+++ lib/subdomain_fu/url_rewriter.rb
@@ -3,7 +3,7 @@ require 'action_dispatch/routing/route_set'
 module ActionDispatch
   module Routing
     class RouteSet #:nodoc:
-      def url_for_with_subdomains(options, path_segments=nil)
+      def url_for_with_subdomains(options, path_segments=nil, x=nil)
         if SubdomainFu.needs_rewrite?(options[:subdomain], (options[:host] || (@request && @request.host_with_port))) || options[:only_path] == false
           options[:only_path] = false if SubdomainFu.override_only_path?
           options[:host] = SubdomainFu.rewrite_host_for_subdomains(options.delete(:subdomain), options[:host] || (@request && @request.host_with_port))
